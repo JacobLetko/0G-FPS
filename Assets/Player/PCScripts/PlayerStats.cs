ebd087846ee12bf94dea1f8d9f687d8e5e5f6ca0
Paul Gibbons <s179054@students.aie.edu.au>
2018-02-26 11:54:52 -0800

    Added bullet sound effects, added player Health(IDamagable), Added ammo(not working)

---
+++ Assets/Player/PCScripts/PlayerStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour, IDamagable
+{
+
+    public float hitPoints = 100;
+    //public float shieldPoints = 100;
+
+
+
+    public void Damage(float amt)
+    {
+        //if (shieldPoints >= 0)
+        //{
+        //    shieldPoints -= amt;
+        //}
+        hitPoints -= amt;
+
+    }
+
+
+
+}

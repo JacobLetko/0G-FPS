732e2d95aa1766001bd32f875812dac33322a78a
Alexander Thomas <s179069@students.aie.edu.au>
2018-02-26 11:18:51 -0800

    Add test reactor

---
+++ Assets/Enemy/ReactorController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactorController : MonoBehaviour, IDamagable
+{
+
+    public float maxHealth = 1000.0f;
+    
+    public AudioClip explodeSound;
+    
+    public AudioSource audioSource;
+    public ParticleSystem explodeEffect;
+
+    private float health;
+    private bool alive = true;
+
+
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+
+
+    public void Damage(float amt)
+    {
+        if (alive)
+        {
+            health = Mathf.Clamp(health - amt, 0.0f, maxHealth);
+            if (health <= 0)
+            {
+                Kill();
+            }
+        }
+    }
+
+    private void Kill()
+    {
+        explodeEffect.Play();
+        audioSource.pitch = 1.0f;
+        audioSource.PlayOneShot(explodeSound);
+        alive = false;
+    }
+
+}

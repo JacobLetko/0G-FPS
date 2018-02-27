9a6b03c1b652af68053c3b24d06a032ae05fc326
Alexander Thomas <s179069@students.aie.edu.au>
2018-02-27 14:27:58 -0800

    Add health pickup prefab

---
+++ Assets/DamageSystem/HealthPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour {
+
+    public float healAmt = 25.0f;
+    public int scoreOnPickup = 10;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerStats player = other.GetComponent<PlayerStats>();
+        if (player)
+        {
+            player.Damage(-healAmt);
+            HighScore.addPoints(scoreOnPickup);
+            gameObject.SetActive(false);
+        }
+    }
+
+}

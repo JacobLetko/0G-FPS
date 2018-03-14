826c6704b982fe44e94c72f6462b7e87e379aa2e
Alexander Thomas <s179069@students.aie.edu.au>
2018-03-14 10:37:57 -0700

    Add objective instructions UI

---
+++ Assets/ui/SimpleFade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleFade : MonoBehaviour {
+
+    public CanvasRenderer[] renderers;
+    public float fadeSpeed = 1.0f;
+    public float startWait = 2.0f;
+
+    private float timer = 0;
+
+
+
+    private void Update()
+    {
+        if(timer < startWait)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            for (int i = 0; i < renderers.Length; ++i)
+            {
+                renderers[i].SetAlpha(renderers[i].GetAlpha() - fadeSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+
+}
